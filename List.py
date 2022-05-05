362e08283ebd0f95397d74e166bf18b8e9cd5437
Raimalsrikanth <96488129+Raimalsrikanth@users.noreply.github.com>
2022-05-05 16:55:11 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,12 @@
+#List of numbers
+print("list1")
+list1 = [12,-7,5,64,-14]
+for i in list1:
+    if i>=0:
+        print(i)
+print(     )
+print("list2")
+list2=[12,14,-95,3]
+for i in list2:
+    if i>=0:
+        print(i)
